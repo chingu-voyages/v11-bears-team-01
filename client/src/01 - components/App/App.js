611e9eb4b3abc779ba52c38c136d1e217cd4a88d
fcastrogarcia <fede.castrog@gmail.com>
2019-09-08 22:56:09 -0300

    Add context setup

--- client/src/01 - components/App/App.js
+++ client/src/01 - components/App/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { AuthProvider } from "../../02 - context/AuthContext";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AuthProvider>
+      <div className="App"></div>;
+    </AuthProvider>
+  );
 }
 
 export default App;
