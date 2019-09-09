49ef6fe3334963afcba6121cc8f388b518e94a8e
fcastrogarcia <fede.castrog@gmail.com>
2019-09-09 19:10:07 -0300

    Backend setup

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PORT: process.env.PORT || 8888, //here should go process.env.PORT
+  DATABASE: process.env.DB_STRING
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const keys = require("./config/keys");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(morgan("dev"));
+
+// Import routes
+const rideRoutes = require("./routes/ride");
+
+// Connecting to the database
+mongoose
+  .connect(keys.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("DB connected!..."));
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes middleware
+app.use("/api", rideRoutes);
+
+// Setting up the port
+const PORT = keys.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
