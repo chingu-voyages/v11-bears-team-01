f4056807bcc002d34017a2a56fdc2fbb78fe4e69
fcastrogarcia <fede.castrog@gmail.com>
2019-09-09 18:59:58 -0300

    Routing setup

--- client/src/01 - components/App/App.js
+++ client/src/01 - components/App/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { AuthProvider } from "../../02 - context/AuthContext";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+//components
+import SignIn from "../Signin/index";
+import SignUp from "../Signup/index";
 import "./App.css";
 
 function App() {
   return (
     <AuthProvider>
-      <div className="App"></div>;
+      <div className="App">
+        <Router>
+          <Route exact path="/" render={() => <Redirect to="/login" />} />
+          <Route exact path="/login" component={SignIn} />
+          <Route exact path="/sign-up" component={SignUp} />
+        </Router>
+      </div>
+      ;
     </AuthProvider>
   );
 }
---
+++ client/src/01 - components/Signup/index.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export default () => {
+  return (
+    <Fragment>
+      <h2>Register</h2>
+    </Fragment>
+  );
+};
