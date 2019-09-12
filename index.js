09a5af114d806f44c27aef64c960c015718b526e
Dizzie42 <dohrann@gmail.com>
2019-09-11 19:44:16 -0500

    server setup code and DB connection up, also added code for body parser

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const keys = require("./config/keys");
+
 const app = express();
 
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
 app.use(morgan("dev"));
+
+//Database Config
+mongoose.connect(process.env.DATABASEURL, {
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('Connected to DB');
+}).catch(err => {
+	console.log('ERROR connecting to database: ', err.message);
+});
+
 
 const PORT = keys.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
