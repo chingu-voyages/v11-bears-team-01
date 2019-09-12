0462840c98f635b6483d5c94bda3c9299fa5f0d6
Dizzie42 <dohrann@gmail.com>
2019-09-11 20:27:20 -0500

    finished backedn auth, postman does not respond when we hit /login or /register

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const keys = require("./config/keys");
+const passport = require("passport");
+const User = require("./models/User");
 
 const app = express();
 
+//BodyParser setup
 app.use(
     bodyParser.urlencoded({
         extended: false
@@ -25,6 +28,11 @@ mongoose.connect(process.env.DATABASEURL, {
 	console.log('ERROR connecting to database: ', err.message);
 });
 
+//Passport config
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", User);
 
+//Start Server
 const PORT = keys.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,8 +7,8 @@ const keys = require("../../config/keys");
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 // Load User model
-const User = require("../../models/User");
-
+const User = require("../models/User");
+console.log("in users file");
 // @route POST api/users/register
 // @desc Register user
 // @access Public
@@ -17,7 +17,6 @@ router.post("/register", (req, res) => {
     if (!isValid) {
         return res.status(400).json(errors);
     }
-
     User.findOne({ email: req.body.email }).then(user => {
         if (user) {
             return res.status(400).json({ email: "Email already exists" });
@@ -42,5 +41,53 @@ router.post("/register", (req, res) => {
     });
 });
 
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+    // Form validation
+    const { errors, isValid } = validateLoginInput(req.body);
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+    // Find user by email
+    User.findOne({ email }).then(user => {
+        // Check if user exists
+        if (!user) {
+            return res.status(404).json({ emailnotfound: "Email not found" });
+        }
+        // Check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+            // Create JWT Payload
+            const payload = {
+                id: user.id,
+                name: user.name
+            };
+            // Sign token
+            jwt.sign(
+                payload,
+                keys.secretOrKey,
+                {
+                    expiresIn: 31556926 // 1 year in seconds
+                },
+                (err, token) => {
+                    res.json({
+                        success: true,
+                        token: "Bearer " + token
+                    });
+                }
+            );
+        } else {
+            return res
+            .status(400)
+            .json({ passwordincorrect: "Password incorrect" });
+            }
+        });
+    });
+});
 
 module.exports = router;
