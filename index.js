8da18cc5f48aeccd94c50513f248fc816df0fa1a
fcastrogarcia <fede.castrog@gmail.com>
2019-09-12 22:34:01 -0300

    Fix server bugs and packages missing

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const keys = require("./config/keys");
+const cors = require("cors");
 const passport = require("passport");
 const users = require("./routes/api/users");
-const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 
-//BodyParser setup
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-);
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use(morgan("dev"));
 
 //Database Config
-mongoose.connect(process.env.DATABASEURL, {
-	useNewUrlParser: true,
-	useCreateIndex: true
-}).then(() => {
-	console.log('Connected to DB');
-}).catch(err => {
-	console.log('ERROR connecting to database: ', err.message);
-});
+mongoose
+  .connect(process.env.DATABASEURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(err => {
+    console.log("ERROR connecting to database: ", err.message);
+  });
 
 //Passport config
 app.use(passport.initialize());
@@ -35,6 +33,5 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 //Start Server
-
 const PORT = keys.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
