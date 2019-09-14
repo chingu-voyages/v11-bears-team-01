66cbcee79f29371d6d80cb3c92ba3b5f4be4a2e0
millord <juancarlosmillord@gmail.com>
2019-09-14 17:37:43 -0400

    add the others parts that form the geo-crub delete and update the Ride

--- controllers/ride.js
+++ controllers/ride.js
@@ -1,7 +1,7 @@
 const Ride = require("../models/Ride");
 
-// Get ride by id
-
+// CRUD FOR THE RIDE MODEL
+// GET THE RIDE BY ID
 exports.rideById = (req, res, next, id) => {
   Ride.findById(id).exec((err, ride) => {
     if (err || !ride) {
@@ -14,8 +14,7 @@ exports.rideById = (req, res, next, id) => {
   });
 };
 
-//Get a single ride
-
+//GET A SINGLE RIDE
 exports.read = (req, res) => {
   return res.json(req.ride);
 };
@@ -37,12 +36,42 @@ exports.create = (req, res) => {
 };
 
 // INDEX GET ALL THE RIDES
-
 exports.list = (req, res) => {
   Ride.find().exec((err, data) => {
     if (err) {
       return res.status(400).json({
         err: err
+      });
+    }
+    res.json(data);
+  });
+};
+
+// DELETE A RIDE
+exports.remove = (req, res) => {
+  let ride = req.ride;
+  ride.remove((err, deletedRide) => {
+    if (err) {
+      return res.status(400).json({
+        err: err
+      });
+    }
+    res.json({
+      message: "Ride deleted successfuly"
+    });
+  });
+};
+
+// UPDATE A RIDE
+exports.update = (req, res) => {
+  const ride = req.ride;
+  ride.name = req.body.name;
+  ride.description = req.body.description;
+  ride.geo = req.body.geo;
+  ride.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: err
       });
     }
     res.json(data);
--- routes/ride.js
+++ routes/ride.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, rideById, read, list } = require("../controllers/ride");
+const {
+  create,
+  rideById,
+  read,
+  list,
+  remove,
+  update
+} = require("../controllers/ride");
 
 router.get("/ride/:rideId", read);
 router.post("/ride/create", create);
+router.delete("/ride/:rideId/:userId", remove);
+router.put("/ride/:rideId/:userId", update);
 router.get("/rides", list);
 
 router.param("rideId", rideById);
