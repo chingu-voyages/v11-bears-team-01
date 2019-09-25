11af538f3f49344db2dfca8e5e40ad1a97255894
millord <juancarlosmillord@gmail.com>
2019-09-25 18:46:24 -0400

    changed the ride model

--- models/ride.js
+++ models/ride.js
@@ -4,16 +4,21 @@ const Schema = mongoose.Schema;
 // Creates a LineString Schema for the bike rides
 
 const RideSchema = new Schema({
-  name1: { type: String, required: true },
-  name2: { type: String, required: true },
-  duration: { type: Number },
-  geo1: {
-    type: { type: String, default: "Point" },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  title: { type: String, required: true },
+
+  waypoints: {
+    type: { type: String, default: "LineString", required: true },
     coordinates: Array
   },
-  geo2: {
-    type: { type: String, default: "Point" },
-    coordinates: Array
+  totalDistance: {
+    type: Number
+  },
+  totalTime: {
+    type: Number
   },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now }
