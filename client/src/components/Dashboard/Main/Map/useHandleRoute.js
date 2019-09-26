eb9688aa8d834ee64cd07a6fddf19833c5cb7b70
fcastrogarcia <fede.castrog@gmail.com>
2019-09-25 22:15:43 -0300

    Add route selection logic

--- client/src/components/Dashboard/Main/Map/useHandleRoute.js
+++ client/src/components/Dashboard/Main/Map/useHandleRoute.js
@@ -1,19 +1,40 @@
 import { useEffect } from "react";
-import L from "leaflet";
 import "leaflet-routing-machine";
+import { setRouteEvent } from "../../../../utils/actions";
 
-export default (routeControl = {}, currentCoords, setRoute) => {
+function filterRouteEventObj(wps, routes) {
+  const waypoints = Object.values(wps).map(wp => [
+    wp.latLng.lat,
+    wp.latLng.lng
+  ]);
+  const totalTime = routes[0].summary.totalTime;
+  const totalDistance = routes[0].summary.totalDistance;
+
+  return {
+    waypoints,
+    totalDistance,
+    totalTime
+  };
+}
+
+export default (routeControl = {}, store, dispatch) => {
+  const { currentCoords, createMode } = store;
+  const wps = createMode ? [currentCoords, currentCoords] : currentCoords;
+
   useEffect(() => {
-    routeControl.current.setWaypoints([
-      L.latLng(currentCoords),
-      L.latLng(currentCoords)
-    ]);
+    routeControl.current.setWaypoints(wps);
+
     function setRouteData(e) {
       const { waypoints, routes } = e;
-      setRoute({ waypoints, routes });
+      const routeData = filterRouteEventObj(waypoints, routes);
+      dispatch(setRouteEvent(routeData));
     }
+
     routeControl.current.addEventListener("routesfound", setRouteData);
     return () =>
       routeControl.current.removeEventListener("routesfound", setRouteData);
   }, [currentCoords]);
 };
+
+//only problem here is that whenever routesfound event is triggered
+//there's a dispatch to context, may bring some perf problems.
--- client/src/components/Dashboard/Sidebar/RidesList/Nav.js
+++ client/src/components/Dashboard/Sidebar/RidesList/Nav.js
@@ -15,7 +15,6 @@ const Ul = styled.ul`
   padding: 0;
   margin: 0;
   color: ${colors.fontBlack};
-  /* background: red; */
 `;
 const Li = styled.li`
   padding: 15px 10px;
@@ -28,22 +27,27 @@ const Li = styled.li`
 `;
 
 export default ({ dispatch, store }) => {
-  const { currentRoute } = store;
-  function handleClick() {
-    //setting waypoints
-    dispatch(setCoords([0, 15]));
+  const { rides } = store;
+
+  function handleClick(e) {
+    const id = e.target.dataset.id;
+    const ride = rides.filter(ride => id === ride._id);
+    //setting currentCoords on map
+    dispatch(setCoords(ride[0].waypoints));
     dispatch(setCreateMode(false));
   }
+
   return (
     <Nav>
       <Ul>
-        <Li onClick={handleClick}>Road X</Li>
-        <Li>Road Y</Li>
-        <Li>Road Z</Li>
-        <Li>Road Z</Li>
+        {rides.map((ride, i) => (
+          <Li key={i} data-id={ride._id} onClick={e => handleClick(e)}>
+            {ride.title}
+          </Li>
+        ))}
       </Ul>
     </Nav>
   );
 };
 
-//fetch all rides from context
+//Gotta check if ObjectID mongoose data type is actually a string.
