5cb490b459b3e3e936b4d0f7a2c3826ce679eed6
fcastrogarcia <fede.castrog@gmail.com>
2019-09-25 22:18:07 -0300

    Add MapController component setup

--- client/src/components/Dashboard/Main/MapController/index.js
+++ client/src/components/Dashboard/Main/MapController/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import SaveChangesButton from "./SaveChangesButton";
 
-export default () => {
-  return <React.Fragment></React.Fragment>;
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 40%;
+`;
+
+export default props => {
+  return (
+    <React.Fragment>
+      <Container>
+        <SaveChangesButton />
+      </Container>
+    </React.Fragment>
+  );
 };
