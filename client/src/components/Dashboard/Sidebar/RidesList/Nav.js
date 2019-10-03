8809c0d57fc0065e2fc60bc340fd5f0346ba70a4
fcastrogarcia <fede.castrog@gmail.com>
2019-10-02 23:05:06 -0300

    Refactor ListButton component

--- client/src/components/Dashboard/Sidebar/RidesList/Nav.js
+++ client/src/components/Dashboard/Sidebar/RidesList/Nav.js
@@ -1,29 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import { updateRoute } from "../../../../utils/actions";
-import { colors } from "../../../../styles/vars";
+import ListButton from "../../../shared/ListButton";
 
 const Nav = styled.nav`
   margin-top: 5px;
 `;
 const Ul = styled.ul`
   list-style: none;
-  text-align: left;
-  font-family: "Archivo", sans-serif;
-  font-weight: 500;
-  font-size: 14px;
   padding: 0;
   margin: 0;
-  color: ${colors.fontBlack};
-`;
-const Li = styled.li`
-  padding: 15px 10px;
-  cursor: pointer;
-  color: rgba(19, 30, 65, 0.6);
-  &:hover {
-    background: rgba(205, 206, 228, 0.4);
-    color: rgba(19, 30, 65, 0.8);
-  }
 `;
 
 export default ({ routeDispatch, rides, setCoords }) => {
@@ -39,9 +25,11 @@ export default ({ routeDispatch, rides, setCoords }) => {
     <Nav>
       <Ul>
         {rides.map((ride, i) => (
-          <Li key={i} data-id={ride._id} onClick={e => handleClick(e)}>
-            {ride.title}
-          </Li>
+          <li key={i}>
+            <ListButton data-id={ride._id} onClick={e => handleClick(e)}>
+              {ride.title}
+            </ListButton>
+          </li>
         ))}
       </Ul>
     </Nav>
