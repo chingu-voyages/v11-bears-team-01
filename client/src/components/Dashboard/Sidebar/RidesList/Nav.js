12e198eef82c520a8ead51d128afd09add14f661
fcastrogarcia <fede.castrog@gmail.com>
2019-10-04 18:32:57 -0300

    Add no rides message in sidebar

--- client/src/components/Dashboard/Sidebar/RidesList/Nav.js
+++ client/src/components/Dashboard/Sidebar/RidesList/Nav.js
@@ -3,6 +3,20 @@ import { updateRoute } from "../../../../utils/actions";
 import ListButton from "../../../shared/ListButton";
 import Ul from "../../../shared/Ul";
 
+const style = {
+  nav: {
+    marginTop: "5px"
+  },
+  ListButton: {
+    margin: "3px 0"
+  },
+  noRides: {
+    textAlign: "left",
+    color: "rgba(19,30,65,0.6)",
+    padding: "10px"
+  }
+};
+
 export default ({ routeStore, routeDispatch, rides, setCoords }) => {
   const { currentRoute } = routeStore;
 
@@ -15,14 +29,15 @@ export default ({ routeStore, routeDispatch, rides, setCoords }) => {
   }
 
   return (
-    <nav style={{ marginTop: "5px" }}>
+    <nav style={style.nav}>
       <Ul>
+        {rides.length < 1 && <h3 style={style.noRides}>No rides to show.</h3>}
         {rides.map((ride, i) => (
           <li key={i}>
             <ListButton
-              style={{ margin: "3px 0" }}
+              style={style.ListButton}
               data-id={ride._id}
-              onClick={e => handleClick(e)}
+              onClick={handleClick}
               selected={currentRoute._id === ride._id ? true : false}
             >
               {ride.title}
