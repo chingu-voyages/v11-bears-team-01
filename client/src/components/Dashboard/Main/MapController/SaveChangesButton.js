c3c70c7e2cb2074dcf269472879ec0922719bf1a
fcastrogarcia <fede.castrog@gmail.com>
2019-10-04 20:27:32 -0300

    Add visual indication on save changes button when its ok

--- client/src/components/Dashboard/Main/MapController/SaveChangesButton.js
+++ client/src/components/Dashboard/Main/MapController/SaveChangesButton.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { PulseLoader } from "react-spinners";
+import { Check } from "styled-icons/boxicons-regular/Check";
 import MainActionButton from "../../../shared/MainActionButton";
 import {
   setRidesData,
@@ -9,25 +11,52 @@ import {
 
 const SaveChangesButton = styled(MainActionButton)`
   min-width: 170px;
+  overflow: hidden;
   width: 190px;
   margin: 0;
+  background: ${props => (props.saved ? "#1BDC77" : "")};
+  border-color: ${props => (props.saved ? "#1BDC77" : "")};
+  transition: all 0.17s ease-out;
 `;
+const Ok = styled(Check)``;
 
 export default ({ dispatch, routeStore, routeDispatch }) => {
   const { createMode, currentRoute } = routeStore;
+  const [loading, setLoading] = useState(false);
+  const [saved, setSaved] = useState(false);
 
   function handleClick() {
     if (!createMode) {
-      dispatch(updateRide(currentRoute));
-      routeDispatch(setCreateModeOff());
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setSaved(true);
+        dispatch(updateRide(currentRoute));
+        routeDispatch(setCreateModeOff());
+        setTimeout(() => {
+          setSaved(false);
+        }, 900);
+      }, 400);
     } else {
-      dispatch(setRidesData(currentRoute));
-      routeDispatch(setCreateModeOff());
+      setLoading(true);
+      setTimeout(() => {
+        dispatch(setRidesData(currentRoute));
+        routeDispatch(setCreateModeOff());
+        setLoading(false);
+        setSaved(true);
+        setTimeout(() => {
+          setSaved(false);
+        }, 900);
+      }, 400);
     }
     //here should go the api call to save ride in db
   }
 
   return (
-    <SaveChangesButton onClick={handleClick}>Save changes</SaveChangesButton>
+    <SaveChangesButton onClick={handleClick} saved={saved ? true : false}>
+      {!loading && !saved && <span>Save changes</span>}
+      {loading && <PulseLoader size={8} color={"#ffffff"} loading={loading} />}
+      {saved && <Ok size={"32px"} />}
+    </SaveChangesButton>
   );
 };
