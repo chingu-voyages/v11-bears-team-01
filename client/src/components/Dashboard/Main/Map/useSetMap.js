9ecc3ec6066525e1a38fc35bf88e93ba5dcd22df
fcastrogarcia <fede.castrog@gmail.com>
2019-10-07 20:22:43 -0300

    Add geocoder style

--- client/src/components/Dashboard/Main/Map/useSetMap.js
+++ client/src/components/Dashboard/Main/Map/useSetMap.js
@@ -19,18 +19,39 @@ export default (coords = []) => {
       ]
     });
     //route mounting
+    let geocoder = L.Control.Geocoder.nominatim();
+
     routeControl.current = L.Routing.control({
       waypoints: coords,
       routeWhileDragging: false,
       routeDragInterval: 0,
       router: L.Routing.mapbox(process.env.REACT_APP_MAPBOX_TOKEN),
       plan: L.Routing.plan([], {
-        geocoder: L.Control.Geocoder.nominatim(),
+        geocoder: geocoder,
         geocodersClassName: "geocoders-container",
-        reverseWaypoints: true
+        reverseWaypoints: true,
+        addButtonClassName: "add-waypoint-btn"
       })
     }).addTo(mapRef.current);
   }, []);
 
   return { mapRef, routeControl };
 };
+
+//nominatim options, this doesnt work
+// {
+//   htmlTemplate: function(r) {
+//     let a = r.address;
+//     const text = L.DomUtil.create(
+//       "p",
+//       "leaflet-control-geocoder-address-detail"
+//     );
+//     console.log(text);
+//     text.innerHTML = `${a.suburb}`;
+//     let string =
+//       "<span class='leaflet-routing-geocoder'>Suburb: {suburb}</span>";
+//     const templatedStr = L.Util.template(string, { suburb: a.suburb });
+//     console.log(templatedStr);
+//     return templatedStr;
+//   }
+// }
