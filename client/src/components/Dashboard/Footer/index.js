45dd990b1e902269eb7686e96366a4f9e54dfac2
fcastrogarcia <fede.castrog@gmail.com>
2019-10-14 12:58:42 -0300

    Add github repo link

--- client/src/components/Dashboard/Footer/index.js
+++ client/src/components/Dashboard/Footer/index.js
@@ -1,20 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import { Github } from "styled-icons/fa-brands/Github";
 
 const Footer = styled.footer`
   height: 3em;
-  background: cyan;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
   position: relative;
   bottom: 0;
   width: 100vw;
+  border-top: 1px solid rgba(205, 206, 228, 0.6);
 `;
 
 export default () => {
   return (
     <React.Fragment>
-      {/* <Footer>
-        <h3>Footer</h3>
-      </Footer> */}
+      <Footer>
+        <a
+          href="https://github.com/chingu-voyages/v11-bears-team-01/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github
+            size={"23px"}
+            color={"rgba(19, 30, 65, 0.8)"}
+            style={{ margin: "17px 40px 0 0" }}
+          />
+        </a>
+      </Footer>
     </React.Fragment>
   );
 };
