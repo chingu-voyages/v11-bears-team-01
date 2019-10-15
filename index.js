912758fb68a4f5cccfaa330e0664cbce53e1ebb1
fcastrogarcia <fede.castrog@gmail.com>
2019-10-15 17:57:55 -0300

    Add index.html file and statics serving & history middleware

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const path = require("path");
 const keys = require("./config/keys");
 const cors = require("cors");
 const passport = require("passport");
+const history = require("connect-history-api-fallback");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use(history());
 
 // Import routes
 const rideRoutes = require("./routes/api/ride");
@@ -37,6 +40,14 @@ require("./config/passport")(passport);
 //Routers
 app.use("/api/users", users);
 app.use("/api", rideRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "client/build/index.html"));
+  });
+}
 
 //Start Server
 const PORT = keys.PORT;
