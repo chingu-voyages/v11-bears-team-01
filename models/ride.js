b9f10fbee0dd750ca979e1864978e93db6193fbd
fcastrogarcia <fede.castrog@gmail.com>
2019-10-14 23:02:05 -0300

    Modify ride schema waypoints data type

--- models/ride.js
+++ models/ride.js
@@ -2,18 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Creates a LineString Schema for the bike rides
-
 const RideSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
     required: true
   },
-  title: { type: String, required: true },
-
+  title: {
+    type: String,
+    required: true
+  },
   waypoints: {
-    type: { type: String, default: "LineString", required: true },
-    coordinates: Array
+    type: [[Number]],
+    required: true
   },
   totalDistance: {
     type: Number
@@ -24,5 +25,4 @@ const RideSchema = new Schema({
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model("Ride", RideSchema);
