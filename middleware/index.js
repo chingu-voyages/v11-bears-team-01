05a6d8b90f29dcb3ee99a7fa7006d5f3a965d0da
fcastrogarcia <fede.castrog@gmail.com>
2019-10-28 10:10:45 -0300

    Delete duplicated middleware in backend

--- middleware/index.js
+++ middleware/index.js
@@ -1,22 +1,7 @@
-const Ride = require("../models/ride");
 const passport = require("passport");
 
 //All middleware goes here
 let middlewareObj = {};
-
-middlewareObj.checkRideOwnership = function(req, res, next) {
-  Ride.findById(req.params.id, (err, foundRide) => {
-    if (err || !foundRide) {
-      console.log("no ride found for " + req.params.id); //temp console log
-      res.json({ error: "No ride" });
-    } else {
-      if (foundRide.author.id.equals(req.user._id)) {
-        //if user owns ride
-        next();
-      }
-    }
-  });
-};
 
 middlewareObj.isLoggedIn = passport.authenticate("jwt", { session: false });
 
